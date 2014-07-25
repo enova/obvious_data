7c3deb4c90a7c107467b5d0571ab9371da035fef
Suan-Aik Yeo <yeosuanaik@gmail.com>
2014-07-25 12:00:14 -0500

    shore up command recorder code

--- lib/obvious_data/command_recorder_methods.rb
+++ lib/obvious_data/command_recorder_methods.rb
@@ -1,10 +1,28 @@
+# At present no obvious_data helpers are auto-invertible. We can't just drop a function
+# executed via #execute_function_file cause that helper is _also_ used to update existing
+# functions. Might address that later.
 module ObviousData::CommandRecorderMethods
-  def execute_function_files(*args)
+  def execute_function_file(*args)
     record(:execute_function_files, args)
   end
 
-  def invert_execute_function_files(args)
-    # FIXME
-    []
+  def drop_function(*args)
+    record(:drop_function, args)
+  end
+
+  def execute_view_file(*args)
+    record(:execute_view_file, args)
+  end
+
+  def drop_view(*args)
+    record(:drop_view, args)
+  end
+
+  def execute_trigger_file(*args)
+    record(:execute_trigger_file, args)
+  end
+
+  def drop_trigger(*args)
+    record(:drop_trigger, args)
   end
 end
